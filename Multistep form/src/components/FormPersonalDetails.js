8b56c237758217a802d6dc92a836987b76353600
Ojaswi Awasthi <ojaswiawasthi2025@gmail.com>
2022-02-16 02:51:31 +0530

    added the basic components

---
+++ Multistep form/src/components/FormPersonalDetails.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class FormPersonalDetails extends Component {
+  render() {
+    return (
+      <div>FormPersonalDetails</div>
+    )
+  }
+}
+
+export default FormPersonalDetails
---
+++ Multistep form/src/components/FormUserDetails.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class FormUserDetails extends Component {
+  render() {
+    return (
+      <div>FormUserDetails</div>
+    )
+  }
+}
+
+export default FormUserDetails
