51cbb7c04c4144b7689564a5df857d8c16a9c407
Ojaswi Awasthi <ojaswiawasthi2025@gmail.com>
2022-02-17 00:40:50 +0530

    added form user details

--- Multistep form/src/components/FormUserDetails.js
+++ Multistep form/src/components/FormUserDetails.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react'
-
+import '../styles.css';
 export class FormUserDetails extends Component {
+
+  continue = e =>{
+    e.preventDefault();
+    this.props.nextStep();
+  }
+
   render() {
+    const {values,handleChange}= this.props;
     return (
-      <div>FormUserDetails</div>
+      <div className='page1'>
+        <h1>Enter user details</h1>
+        <input 
+        placeholder='Enter first Name'
+        onChange={handleChange('firstName')} required
+        />
+        <br/>
+    
+        <input 
+        placeholder='Enter Last Name'
+        onChange={handleChange('lastName')} required
+        />
+        <br/>
+        <input 
+        placeholder='Enter Email '
+        onChange={handleChange('email')} required
+        />
+        <br/>
+        <button onClick={this.continue}>Continue</button>
+      </div>
     )
   }
 }
 
 export default FormUserDetails
+
