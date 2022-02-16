ae720c705a9ca85c076556852d4b259592afbd7b
Ojaswi Awasthi <ojaswiawasthi2025@gmail.com>
2022-02-17 00:41:46 +0530

    Update FormPersonalDetails.js

--- Multistep form/src/components/FormPersonalDetails.js
+++ Multistep form/src/components/FormPersonalDetails.js
@@ -1,11 +1,45 @@
 import React, { Component } from 'react'
-
 export class FormPersonalDetails extends Component {
+
+  continue= e=>{
+    e.preventDefault();
+    this.props.nextStep();
+  }
+
+  back= e=>{
+    e.preventDefault();
+    this.props.prevStep();
+  }
+
   render() {
+    const {values,handleChange}=this.props;
     return (
-      <div>FormPersonalDetails</div>
+      
+        <div className='page1'>
+        <h1>Enter Personal Details</h1>
+        <input 
+        placeholder='Enter occupation'
+        onChange={handleChange('occupation')} required
+        />
+        <br/>
+    
+        <input 
+        placeholder='Enter city name'
+        onChange={handleChange('city')} required
+        />
+        <br/>
+        <input 
+        placeholder='Enter bio '
+        onChange={handleChange('bio')} required
+        />
+        <br/>
+        <button onClick={this.continue}>Continue</button>
+        <button onClick={this.back}
+        style={{backgroundColor:'transparent'}}
+        >Back</button>
+      </div>
     )
   }
 }
 
-export default FormPersonalDetails
+export default FormPersonalDetails;
